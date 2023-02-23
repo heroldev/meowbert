ea7af27681f12bae10214b1c7f09985e671981f2
Andrew Herold <heroldaksd@gmail.com>
2023-02-23 14:54:09 -0600

    version fix

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -17,7 +17,7 @@ export const Help: Command = {
         { name: 'Question of the Day Commands (Admin Only)', value: '`/addqotd` - adds a new Question of the Day\n`/startqotd` - enables the Question of the Day and sets the channel where it will be sent\n`/stopqotd` - disables the Question of the Day' },
         { name: 'Developer Commands', value: '`/newqotd` - forcibly sends a new QOTD, regardless of time\n`/redoqotd` - sends the last-used QOTD\n`/broadcast` - sends a custom message to all QOTD channels'},
         )
-      .setFooter({ text: 'meowbert v1.3.0', iconURL: 'https://i.imgur.com/QuUEvbD.png' });
+      .setFooter({ text: 'meowbert v1.4.0', iconURL: 'https://i.imgur.com/QuUEvbD.png' });
 
     await interaction.followUp({
       ephemeral: true,
